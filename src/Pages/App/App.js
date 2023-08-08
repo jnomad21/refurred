c43db3c89e8635212196b6c1e7ee33c5cf9e0cb6
Joel <JoelFuelling@gmail.com>
2023-08-08 14:47:36 -0500

    Moved <LoadScript>, LoadScript import, and gKey variable to App.js so they render only once on appload instead of each time the map is called

--- src/Pages/App/App.js
+++ src/Pages/App/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
+import { LoadScript } from '@react-google-maps/api';
 import GoogleMaps from '../GoogleMaps/GoogleMaps';
 import HomePage from '../HomePage/HomePage';
 import NewDogPage from '../NewDogPage/NewDogPage';
@@ -12,15 +13,16 @@ import DogDetailPage from '../DogDetailPage/DogDetailPage'
 import QuizPage from '../QuizPage/QuizPage';
 import EditDogPage from '../EditDogPage/EditDogPage';
 
+const googleKey = process.env.REACT_APP_GOOGLE;
 export default function App() {
   const [user, setUser] = useState(getUser());
 
 
   return (
     <>
+    <LoadScript googleMapsApiKey={googleKey}>
       <NavBar />
       <Routes>
-    
         <Route path="/map" element={<GoogleMaps/>} />
         <Route path="/" element={<HomePage />} />
         <Route path="/dogs" element={<DogIndexPage />} />
@@ -30,6 +32,7 @@ export default function App() {
         <Route path="/dogs/auth" element={<AuthPage setUser={setUser} />} />
         <Route path="/dogs/:dogId/edit" element={<EditDogPage />} />
       </Routes>
+    </LoadScript>
     </>
   );
 }
