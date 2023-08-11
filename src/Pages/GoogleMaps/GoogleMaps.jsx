0d3a93af2c6c8556c1878259008c598cfd1ef160
Joel <JoelFuelling@gmail.com>
2023-08-11 15:56:26 -0500

    Setup starting component structure for Breeder Filter on GoogleMaps component

--- src/Pages/GoogleMaps/GoogleMaps.jsx
+++ src/Pages/GoogleMaps/GoogleMaps.jsx
@@ -10,12 +10,16 @@ const containerStyle = {
 
 
 export default function GoogleMaps() {
+    // Set the distance state starting at 5
+    const [selectedDistance, setSelectedDistance] = useState(5)
     const [center, setCenter] = useState({
         lat: 41.8220656,
         lng: -88.440897,
     });
 
     const [userLocation, setUserLocation] = useState(null);
+
+    
 
     useEffect(() => {
         // Request geolocation permission
@@ -38,6 +42,34 @@ export default function GoogleMaps() {
 
     return (
         <>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1>Find a nearby Breeder:</h1>
+        <form className="">
+            <div className="form-group mb-3">Within how many miles?
+                <select value={selectedDistance} onChange = {(e) => setSelectedDistance(Number(e.target.value))} name="" className="form-control">
+                    <option value={5}>5 miles</option>
+                    <option value={10}>10 miles</option>
+                    <option value={25}>25 miles</option>
+                    <option value={50}>50 miles</option>
+                    <option value={250}>51-250 miles</option>
+                </select>
+            </div>
+            <div className="form-group mb-3">For which breeds?
+                <select className="form-control">
+                {/* Return list of breeds within 'setSelected' value from the users current lat/lang (Upon form submission?). Then, filter to return the list closest to furthest */}
+                    <option value={1}>Shnouzer</option>
+                    <option value={2}>Laberdoodle</option>
+                    <option value={3}>German Sheppard</option>
+                    <option value={4}>Dauchsund</option>
+                    <option value={5}>Collie</option>
+                </select>
+            </div>
+        </form>
+        
             <GoogleMap
                 mapContainerStyle={containerStyle}
                 center={center}
