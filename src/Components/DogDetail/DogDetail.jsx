ce6fceac9ae7b413379f8f67e60deb27b53dc356
Jason McGrath <jnomad21@yahoo.com>
2023-08-21 16:33:40 -0700

    dog image template literals

--- src/Components/DogDetail/DogDetail.jsx
+++ src/Components/DogDetail/DogDetail.jsx
@@ -51,9 +51,9 @@ export default function DogDetail({ dog, handleDelete, setDog }) {
   return (
     <div className="container mt-5 detail-container">
       <h1 className="dog-breed-title">{dog.breed}</h1>
-      {/* <div className="dog-image">
-        <img src={dog.image} alt={dog.breed} />
-      </div> */}
+      <div className="dog-image">
+      <img src={`https://pet-app.s3.us-west-1.amazonaws.com/${encodeURIComponent(dog.breed)}.jpg`} alt={`${dog.breed}`}/>
+      </div>
       <div className="dog-about lead mb-5">
         <p>{dog.about}</p>
       </div>
