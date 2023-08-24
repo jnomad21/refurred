5ea3a316b4f9f788bad46d137102b9ea2a9b227e
Jason McGrath <jnomad21@yahoo.com>
2023-08-24 15:01:02 -0700

    added auto complete search filter for index page and css file

--- src/Components/DogAutoCompleteFilter/DogAutoCompleteFilter.jsx
+++ src/Components/DogAutoCompleteFilter/DogAutoCompleteFilter.jsx
@@ -1,4 +1,5 @@
 import { ReactSearchAutocomplete } from 'react-search-autocomplete';
+import './DogAutoCompleteFilter.css'
 
 export default function DogAutoCompleteFilter({ dogs, handleFilter }) {
   const handleSelect = (selected) => {
@@ -8,11 +9,15 @@ export default function DogAutoCompleteFilter({ dogs, handleFilter }) {
   const items = dogs.map((dog) => ({ id: dog._id, name: dog.breed }));
 
   return (
-    <ReactSearchAutocomplete
-      items={items}
-      onSelect={handleSelect}
-      autoFocus
-      placeholder="Search for a breed..."
-    />
+   <div className='index-auto-filter-container'>
+        <div className='index-auto-filter'>
+            <ReactSearchAutocomplete
+            items={items}
+            onSelect={handleSelect}
+            autoFocus
+            placeholder="Search for a breed..."
+            />
+        </div>
+    </div> 
   );
 }
