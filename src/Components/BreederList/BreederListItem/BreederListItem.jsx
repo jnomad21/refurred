a390622e19dfcde9cd27debc2ce92e1745be8e6a
Joel <JoelFuelling@gmail.com>
2023-08-24 16:01:49 -0500

    Added some classes to breeders page. Added website link ***DOES NOT WORK unless the address entered followed 'https://www.website.com format'***

--- src/Components/BreederList/BreederListItem/BreederListItem.jsx
+++ src/Components/BreederList/BreederListItem/BreederListItem.jsx
@@ -1,16 +1,26 @@
 import { Link } from 'react-router-dom';
+import './BreederListItem.css'
 
 export default function BreederListItem({breeder}){
     return(
         <>
-            <h5>Breeder: 
-             <span><Link to={`/dogs/breeders/${breeder._id}`}>
-                 {breeder.orgName} 
-            </Link></span></h5>
-            <h5>Type: {breeder.breed}</h5>
-            <h5>State: {breeder.state}</h5>
-            <br />
-            <br />
+        <div className="col-md-4 mb-4">
+            <div className="card card-fluid index-card">
+                <div className="card-body">
+                <h5 className="card-title">
+                    <span>
+                        <Link className="breeder-links"to={`/dogs/breeders/${breeder._id}`}>
+                        {breeder.orgName} 
+                        </Link>
+                    </span>
+                </h5>
+                <h5>Type: {breeder.breed}</h5>
+                <h5>Location: {breeder.state}</h5>
+                <h5><a href={`${breeder.website}`} rel="noreferrer" target="_blank"> Website</a>
+                </h5>
+                </div>
+            </div>
+        </div>   
         </>
     )
 }
