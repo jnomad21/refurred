71f874dc9b1caadb7614ed4408c01b7d17923951
Joel <JoelFuelling@gmail.com>
2023-08-24 18:35:39 -0500

    committing

--- src/Pages/GoogleMaps/GoogleMaps.jsx
+++ src/Pages/GoogleMaps/GoogleMaps.jsx
@@ -142,22 +142,6 @@ function Map() {
         <div className="row featurette">
             <div className="col-md-3">
             <BreederPage/>
-             {/*
-                <BreedersByMiles breeders={setBreeders}/>
-                1 - Return selectable Markers of breeders within 'setSelectedDistance' value from the users current lat/lang. Separate list on side (if we build one) needs sorted by closest to furthest, top to bottom, with distance/miles displayed clearly. 
-                    1.1 - On the map, and ideally, these would have a popup 'card' with some basic info and website/contact to click on hover.
-                    1.2 - Google: How to convert the differences between lat and lng into miles?
-                2 - Search button will need 'onSubmit' or similar. It will then render the component returning breeders
-                3 - Best way to generate a reliable list of breeders from all over the country (maybe world, but we'll do country to start)...
-                
-                 <--- COMPONENT MOCK UP --->
-                {breeders.map((breeder, index) => (
-                <Marker
-                key={index}
-                position={{ lat: breeder.lat, lng: breeder.lng }} 
-                />
-                ))} 
-                */}
           </div>
           <div className="col-md-9">
             <GoogleMap
