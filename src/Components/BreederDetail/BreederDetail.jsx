b197ef53fc1ddd6652a2bc52fe7ae105422377f4
Kristin Stockley <kbstockley@gmail.com>
2023-08-31 15:45:10 -0500

    breeder index and detail temporary styling

--- src/Components/BreederDetail/BreederDetail.jsx
+++ src/Components/BreederDetail/BreederDetail.jsx
@@ -1,15 +1,38 @@
-export default function BreederDetail({breeder, setBreeder}){
-    return(
-    <>
-    <main>
-    <h4>Breeder Name:{breeder.orgName}</h4>
-    <h4>Breeds Available:{breeder.breed}</h4>
-    <h4>Address:{breeder.address}</h4>
-    <h4>State:{breeder.state}</h4>
-    <h4>Zip Code:{breeder.zipCode}</h4>
-    <h4>Phone Number:{breeder.phone}</h4>
-    <h4>Breeder Website:{breeder.website}</h4>
-    </main>
-    </>
-    )
+import './BreederDetail.css';
+
+export default function BreederDetail({ breeder }) {
+  return (
+    <div className="container breeder-detail-container">
+      <h1 className="text-center mb-4">{breeder.orgName}</h1>
+      <div className="card p-4">
+        <div className="row">
+          <div className="col">
+            <div className="breeder-details">
+              <div className="breeder-info">
+                <h4 className="text-center mb-2 breeder-detail-title">Breeds Available:</h4>
+                <p className="text-center">{breeder.breed}</p>
+                <h4 className="text-center mb-2 breeder-detail-title">Address:</h4>
+                <p className="text-center">
+                  {breeder.address} <br />
+                  {breeder.state}, {breeder.zipCode}
+                </p>
+                <h4 className="text-center mb-2 breeder-detail-title">Phone Number:</h4>
+                <p className="text-center">{breeder.phone}</p>
+                <h4 className="text-center mb-2 breeder-detail-title">Website:</h4>
+                <p className="text-center">
+                  <a
+                    href={breeder.website}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {breeder.website}
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/Components/BreederList/BreederListItem/BreederListItem.jsx
+++ src/Components/BreederList/BreederListItem/BreederListItem.jsx
@@ -1,26 +1,28 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
-import './BreederListItem.css'
+import './BreederListItem.css';
 
-export default function BreederListItem({breeder}){
-    return(
-        <>
-        <div className="col-md-4 mb-4">
-            <div className="card card-fluid index-card">
-                <div className="card-body">
-                <h5 className="card-title">
-                    <span>
-                        <Link className="breeder-links"to={`/dogs/breeders/${breeder._id}`}>
-                        {breeder.orgName} 
-                        </Link>
-                    </span>
-                </h5>
-                <h5>Type: {breeder.breed}</h5>
-                <h5>Location: {breeder.state}</h5>
-                <h5><a href={`${breeder.website}`} rel="noreferrer" target="_blank"> Website</a>
-                </h5>
-                </div>
-            </div>
-        </div>   
-        </>
-    )
+export default function BreederListItem({ breeder }) {
+  return (
+    <div className="col-md-12 d-flex justify-content-center mb-4">
+      <div className="card breeder-card-fluid breeder-list-card">
+        <div className="card-body">
+          <h5 className="card-title mt-0">
+            <Link className="breeder-links" to={`/dogs/breeders/${breeder._id}`}>
+              {breeder.orgName}
+            </Link>
+          </h5>
+          <h5>Breeds: {breeder.breed}</h5>
+          <h5>Location: {breeder.state}</h5>
+          {/* <h5>
+            <a href={breeder.website} rel="noreferrer" target="_blank">
+              Website
+            </a>
+          </h5> */}
+
+          <Link className="btn btn-success" to={`/dogs/breeders/${breeder._id}`}>Details</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
