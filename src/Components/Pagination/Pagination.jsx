09afd564cf457a3c390cdd77c2dcb34063aeec1f
Jason McGrath <jnomad21@yahoo.com>
2023-09-07 13:09:11 -0700

    added pagination

---
+++ src/Components/Pagination/Pagination.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import './Pagination.css'
+
+export default function Pagination({
+  currentPage,
+  totalPages,
+  onPageChange,
+}) {
+  const handlePageChange = (page) => {
+    onPageChange(page);
+  };
+
+  const isFirstPage = currentPage === 1;
+  const isLastPage = currentPage === totalPages;
+
+  return (
+    <nav className='pag-nav .btn-outline-primary'>
+      <ul className='pagination .btn-outline-primary'>
+        <li >
+          <button
+            className="page-link"
+            onClick={() => handlePageChange(1)}
+            disabled={isFirstPage}
+          >
+            First
+          </button>
+        </li>
+        <li >
+          <button
+            className="page-link"
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={isFirstPage}
+          >
+            Previous
+          </button>
+        </li>
+        <li>
+          <button className="page-link" disabled>
+            {currentPage} of {totalPages}
+          </button>
+        </li>
+        <li >
+          <button
+            className="page-link"
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={isLastPage}
+          >
+            Next
+          </button>
+        </li>
+        <li >
+          <button
+            className="page-link"
+            onClick={() => handlePageChange(totalPages)}
+            disabled={isLastPage}
+          >
+            Last
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+}
